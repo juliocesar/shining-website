115f33e1cf374d029322462ecb4b46706fd643cc
Julio Cesar Ody <julio.ody@nexuspoint.com.au>
2010-04-09 15:09:48 +1000

    slide styles, fixed TOC generated with itself in it

--- public/js/dascripts.js
+++ public/js/dascripts.js
@@ -10,11 +10,11 @@ $(document).ready(function() {
   
   // Generates a nice looking table of contents
   $('header').after('<section id="toc"><ol></ol></section>');  
-  $('#toc').prepend('<h1>Table of contents</h1>');
   $('section h1').each(function() {
     var container = $(this).parents(':first'), text = $(this).text();
     $('#toc ol').append('<li style="display: none"><a href="#' + container.attr('id') + '">' + text + '</a></li>')
   });
+  $('#toc').prepend('<h1>Table of contents</h1>');  
   $('#toc ol li').oneAtEachTime('fadeIn', [], 100);
   
   // Under construction
