f13e2143f1fe5d3573a03a3070ceecc9ee23ab07
Julio Cesar Ody <julioody@gmail.com>
2010-07-12 15:57:14 +1000

    and strike 3, you're out

--- public/js/dascripts.js
+++ public/js/dascripts.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
   // Generates a nice looking table of contents
   $('body > header').after('<section id="toc"><ol></ol></section>');  
   $('section header h1').each(function() {
-    var container = $(this).parents(':first'), text = $(this).text();
+    var container = $(this).parents('section'), text = $(this).text();
     $('#toc ol').append('<li><a href="#' + container.attr('id') + '">' + text + '</a></li>')
   });
   $('#toc').prepend('<h1>Table of contents</h1>');  
