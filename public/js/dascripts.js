4b453cd0c2ed22c6424f3d624ae0c447d94fdda3
Julio Cesar Ody <julioody@gmail.com>
2010-07-12 15:51:36 +1000

    whoops!

--- public/js/dascripts.js
+++ public/js/dascripts.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   // Generates a nice looking table of contents
-  $('header').after('<section id="toc"><ol></ol></section>');  
+  $('body header').after('<section id="toc"><ol></ol></section>');  
   $('section h1').each(function() {
     var container = $(this).parents(':first'), text = $(this).text();
     $('#toc ol').append('<li><a href="#' + container.attr('id') + '">' + text + '</a></li>')
